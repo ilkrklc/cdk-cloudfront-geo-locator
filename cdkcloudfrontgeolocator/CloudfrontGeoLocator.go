73aab28d6ee612fd405e2cb9697475d9ce751447
ilkrklc <ilkrklc@gmail.com>
2024-01-02 06:28:09 +0000

    chore(release): v0.1.12

---
+++ cdkcloudfrontgeolocator/CloudfrontGeoLocator.go
@@ -0,0 +1,193 @@
+package cdkcloudfrontgeolocator
+
+import (
+	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
+	_init_ "github.com/ilkrklc/cdk-cloudfront-geo-locator/cdkcloudfrontgeolocator/jsii"
+
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/ilkrklc/cdk-cloudfront-geo-locator/cdkcloudfrontgeolocator/internal"
+)
+
+// Experimental.
+type CloudfrontGeoLocator interface {
+	constructs.Construct
+	// The ID of the CloudFront cache policy.
+	// Experimental.
+	CloudfrontCachePolicyId() *string
+	// The ID of the CloudFront origin request policy.
+	// Experimental.
+	CloudfrontOriginRequestPolicyId() *string
+	// The domain name of the CloudFront distribution.
+	// Experimental.
+	DistributionDomainName() *string
+	// The ID of the CloudFront distribution.
+	// Experimental.
+	DistributionId() *string
+	// The ARN of the Lambda function.
+	// Experimental.
+	LambdaFunctionArn() *string
+	// The current version of the Lambda function.
+	// Experimental.
+	LambdaFunctionVersion() awslambda.Version
+	// The tree node.
+	// Experimental.
+	Node() constructs.Node
+	// The ARN of the S3 bucket.
+	// Experimental.
+	S3BucketArn() *string
+	// Returns a string representation of this construct.
+	// Experimental.
+	ToString() *string
+}
+
+// The jsii proxy struct for CloudfrontGeoLocator
+type jsiiProxy_CloudfrontGeoLocator struct {
+	internal.Type__constructsConstruct
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) CloudfrontCachePolicyId() *string {
+	var returns *string
+	_jsii_.Get(
+		j,
+		"cloudfrontCachePolicyId",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) CloudfrontOriginRequestPolicyId() *string {
+	var returns *string
+	_jsii_.Get(
+		j,
+		"cloudfrontOriginRequestPolicyId",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) DistributionDomainName() *string {
+	var returns *string
+	_jsii_.Get(
+		j,
+		"distributionDomainName",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) DistributionId() *string {
+	var returns *string
+	_jsii_.Get(
+		j,
+		"distributionId",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) LambdaFunctionArn() *string {
+	var returns *string
+	_jsii_.Get(
+		j,
+		"lambdaFunctionArn",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) LambdaFunctionVersion() awslambda.Version {
+	var returns awslambda.Version
+	_jsii_.Get(
+		j,
+		"lambdaFunctionVersion",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) Node() constructs.Node {
+	var returns constructs.Node
+	_jsii_.Get(
+		j,
+		"node",
+		&returns,
+	)
+	return returns
+}
+
+func (j *jsiiProxy_CloudfrontGeoLocator) S3BucketArn() *string {
+	var returns *string
+	_jsii_.Get(
+		j,
+		"s3BucketArn",
+		&returns,
+	)
+	return returns
+}
+
+
+// Experimental.
+func NewCloudfrontGeoLocator(scope constructs.Construct, id *string, props *CloudfrontGeoLocatorProps) CloudfrontGeoLocator {
+	_init_.Initialize()
+
+	if err := validateNewCloudfrontGeoLocatorParameters(scope, id, props); err != nil {
+		panic(err)
+	}
+	j := jsiiProxy_CloudfrontGeoLocator{}
+
+	_jsii_.Create(
+		"cdk-cloudfront-geo-locator.CloudfrontGeoLocator",
+		[]interface{}{scope, id, props},
+		&j,
+	)
+
+	return &j
+}
+
+// Experimental.
+func NewCloudfrontGeoLocator_Override(c CloudfrontGeoLocator, scope constructs.Construct, id *string, props *CloudfrontGeoLocatorProps) {
+	_init_.Initialize()
+
+	_jsii_.Create(
+		"cdk-cloudfront-geo-locator.CloudfrontGeoLocator",
+		[]interface{}{scope, id, props},
+		c,
+	)
+}
+
+// Checks if `x` is a construct.
+//
+// Returns: true if `x` is an object created from a class which extends `Construct`.
+// Deprecated: use `x instanceof Construct` instead.
+func CloudfrontGeoLocator_IsConstruct(x interface{}) *bool {
+	_init_.Initialize()
+
+	if err := validateCloudfrontGeoLocator_IsConstructParameters(x); err != nil {
+		panic(err)
+	}
+	var returns *bool
+
+	_jsii_.StaticInvoke(
+		"cdk-cloudfront-geo-locator.CloudfrontGeoLocator",
+		"isConstruct",
+		[]interface{}{x},
+		&returns,
+	)
+
+	return returns
+}
+
+func (c *jsiiProxy_CloudfrontGeoLocator) ToString() *string {
+	var returns *string
+
+	_jsii_.Invoke(
+		c,
+		"toString",
+		nil, // no parameters
+		&returns,
+	)
+
+	return returns
+}
+
---
+++ cdkcloudfrontgeolocator/CloudfrontGeoLocatorProps.go
@@ -0,0 +1,67 @@
+package cdkcloudfrontgeolocator
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudfront"
+)
+
+// Properties for the CloudfrontGeoLocator construct.
+// Experimental.
+type CloudfrontGeoLocatorProps struct {
+	// The AWS account ID this resource belongs to.
+	// Default: - the resource is in the same account as the stack it belongs to.
+	//
+	// Experimental.
+	Account *string `field:"optional" json:"account" yaml:"account"`
+	// ARN to deduce region and account from.
+	//
+	// The ARN is parsed and the account and region are taken from the ARN.
+	// This should be used for imported resources.
+	//
+	// Cannot be supplied together with either `account` or `region`.
+	// Default: - take environment from `account`, `region` parameters, or use Stack environment.
+	//
+	// Experimental.
+	EnvironmentFromArn *string `field:"optional" json:"environmentFromArn" yaml:"environmentFromArn"`
+	// The value passed in by users to the physical name prop of the resource.
+	//
+	// - `undefined` implies that a physical name will be allocated by
+	//   CloudFormation during deployment.
+	// - a concrete value implies a specific physical name
+	// - `PhysicalName.GENERATE_IF_NEEDED` is a marker that indicates that a physical will only be generated
+	//   by the CDK if it is needed for cross-environment references. Otherwise, it will be allocated by CloudFormation.
+	// Default: - The physical name will be allocated by CloudFormation at deployment time.
+	//
+	// Experimental.
+	PhysicalName *string `field:"optional" json:"physicalName" yaml:"physicalName"`
+	// The AWS region this resource belongs to.
+	// Default: - the resource is in the same region as the stack it belongs to.
+	//
+	// Experimental.
+	Region *string `field:"optional" json:"region" yaml:"region"`
+	// A unique name to identify the cloudfront cache policy.
+	// Default: - CloudfrontGeoLocatorCachePolicy.
+	//
+	// Experimental.
+	CloudfrontCachePolicyName *string `field:"optional" json:"cloudfrontCachePolicyName" yaml:"cloudfrontCachePolicyName"`
+	// A unique name to identify the cloudfront origin request policy.
+	// Default: - CloudfrontGeoLocatorOriginRequestPolicy.
+	//
+	// Experimental.
+	CloudfrontOriginRequestPolicyName *string `field:"optional" json:"cloudfrontOriginRequestPolicyName" yaml:"cloudfrontOriginRequestPolicyName"`
+	// The price class for the CloudFront distribution.
+	// Default: - PRICE_CLASS_100.
+	//
+	// Experimental.
+	CloudfrontPriceClass awscloudfront.PriceClass `field:"optional" json:"cloudfrontPriceClass" yaml:"cloudfrontPriceClass"`
+	// A unique name to identify the lambda function.
+	// Default: - cloudfront-geo-locator.
+	//
+	// Experimental.
+	LambdaFunctionName *string `field:"optional" json:"lambdaFunctionName" yaml:"lambdaFunctionName"`
+	// A unique name to identify the s3 bucket.
+	// Default: - cloudfront-geo-locator-origin.
+	//
+	// Experimental.
+	S3BucketName *string `field:"optional" json:"s3BucketName" yaml:"s3BucketName"`
+}
+
